0fcfa34d8ec1eb4672c0c128af6f411c477bd730
Ran Hershko <ran.hershko@gmail.com>
2020-02-24 23:15:45 +0200

    create k8s management apps ssl secret

---
+++ shell/create-k8s-secret-app-ssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+app=$1
+
+#kubectl create secret generic vault-tls -n $(namespace) \
+  #--from-file="${DIR}/ca.crt" \
+  #--from-file="vault.crt=${DIR}/vault-combined.crt" \
+  #--from-file="vault.key=${DIR}/vault.key"
+
+if [ "$app" == "vault" ]
+then
+  kubectl create secret generic "${app}-tls" -n "${app}" \
+    --from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
+    --from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
+else
+  kubectl create secret generic "${app}-tls" -n default \
+    --from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
+    --from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
+fi
