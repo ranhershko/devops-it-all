fcde251584e2a6b076a374fba45580bced562665
Ran Hershko <ran.hershko@gmail.com>
2020-02-24 22:35:36 +0200

    Change eks-creds secret creation to vault namespace

--- shell/create-k8s-secret-aws-keys.sh
+++ shell/create-k8s-secret-aws-keys.sh
@@ -1,2 +1,2 @@
-#! /bin/bash -xv
-kubectl create secret generic eks-creds --from-literal="AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" --from-literal="AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
+#! /bin/bash
+kubectl create secret generic eks-creds -n vault --from-literal="AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" --from-literal="AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
