40167c54e7b5c27b62e1de9c824f3010b94a59dd
Ran Hershko <ran.hershko@gmail.com>
2020-03-11 02:30:45 +0200

    modified:   terraform/aws/vpc-n-eks-devops-it-all/create-vpc-n-eks-backend.tf

--- terraform/aws/vpc-n-eks-devops-it-all/create-vpc-n-eks-backend.tf
+++ terraform/aws/vpc-n-eks-devops-it-all/create-vpc-n-eks-backend.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 0.12.0"
   backend "s3" {}
-#    encrypt = true
-#    bucket  = "devopsitall-terraform-remote-state"
-#    key     = "vpc-n-eks-terraform.tfstate"
-##    region  = "us-east-1"
-#
- #   dynamodb_table = "devopsitall-vpc-n-eks-terraform-remote-lock"
+    #encrypt = true
+    #bucket  = "devopsitall-terraform-remote-state"
+    #key     = "vpc-n-eks-terraform.tfstate"
+    #region  = "us-east-1"
+
+    #dynamodb_table = "devopsitall-vpc-n-eks-terraform-remote-lock"
  #} 
 }
