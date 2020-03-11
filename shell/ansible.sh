68cce7571d7e7f7fa7a13a52bcba353f95ace2dd
Ran Hershko <ran.hershko@gmail.com>
2020-03-11 02:43:06 +0200

    add jq & dnsmasq installation

--- shell/ansible.sh
+++ shell/ansible.sh
@@ -5,4 +5,6 @@ sudo dnf -y install python2
 sudo dnf -y install python2-pip
 sudo dnf -y install python3
 sudo dnf -y install python3-pip
+sudo dnf -y install jq
 sudo -H pip3 install ansible
+sudo -H pip3 install dnsmasq
