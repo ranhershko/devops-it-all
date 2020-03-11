a4955f55e139c015681d88c46e69327c27881240
Ran Hershko <ran.hershko@gmail.com>
2020-03-11 02:34:25 +0200

    modified:   terraform/aws/haproxy-autoscale/create-autoscale-haproxy.tf

--- terraform/aws/haproxy-autoscale/create-autoscale-haproxy.tf
+++ terraform/aws/haproxy-autoscale/create-autoscale-haproxy.tf
@@ -6,34 +6,102 @@
   #}
 #}
 
-resource "aws_launch_configuration" "haproxy" {
-  name_prefix                 = "haproxy_launch_configuration_devopsitall-"
-  image_id                    = data.aws_ami.devops-it-all-consulNhaproxy-AMI.id
-  instance_type               = var.haproxy_instance_type
-  key_name                    = "devopsitall"
-  security_groups             = [data.terraform_remote_state.vpc-n-eks.outputs.aws_worker_security_group]
-  #associate_public_ip_address = true
+resource "aws_iam_role" "haproxy_ec2" {
+  name = "haproxy_ec2_role"
 
-  user_data_base64            = data.template_cloudinit_config.haproxy_userdata.renered
-  ##cloud-config
-  #runcmd:
-    #- aws ec2 associate-address --instance-id $(curl http://169.254.169.254/latest/meta-data/instance-id) --allocation-id ${data.aws_eip.haproxy.id} --allow-reassociation
-  #EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com",
+        "Service": "eks.amazonaws.com",
+        "Service": "sts.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "haproxy_ec2" {
+  name = "haproxy_ec2_role_policy"
+  role = aws_iam_role.haproxy_ec2.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "eks:ListTagsForResource",
+                "eks:DescribeUpdate",
+                "eks:ListUpdates",
+                "eks:DescribeCluster"
+            ],
+            "Resource": "arn:aws:eks:*:*:cluster/*"
+        },
+        {
+            "Sid": "VisualEditor2",
+            "Effect": "Allow",
+            "Action": "eks:ListClusters",
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor3",
+            "Effect": "Allow",
+            "Action": "ec2:AssociateAddress",
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor4",
+            "Effect": "Allow",
+            "Action": "sts:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "haproxy_ec2" {
+  name = "haproxy_ec2_iam_instance_profile"
+  role = aws_iam_role.haproxy_ec2.name
+}
+
+resource "aws_launch_configuration" "haproxy" {
+  name_prefix                  = "haproxy_launch_configuration_devopsitall-"
+  image_id                     = data.aws_ami.devops-it-all-consulNhaproxy-AMI.id
+  instance_type                = var.haproxy_instance_type
+  key_name                     = "devopsitall"
+  security_groups              = [data.terraform_remote_state.vpc-n-eks.outputs.aws_worker_security_group]
+  associate_public_ip_address  = true
+  iam_instance_profile         = aws_iam_instance_profile.haproxy_ec2.name 
+  user_data                    = data.template_cloudinit_config.haproxy_userdata.rendered
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "haproxy" {
-  name                  = "haproxy_autoscaling_devopsitall"
-  launch_configuration  = aws_launch_configuration.haproxy.name
-  vpc_zone_identifier   = data.terraform_remote_state.vpc-n-eks.outputs.aws_subnet_ids_public
-  min_size              = var.haproxy_scale_size
-  max_size              = var.haproxy_scale_size
-  desired_capacity      = var.haproxy_scale_size
-  health_check_type     = "EC2"
-  force_delete          = true
-  
+  name                    = "haproxy_autoscaling-devops-it-all"
+  launch_configuration    = aws_launch_configuration.haproxy.name
+  vpc_zone_identifier     = data.terraform_remote_state.vpc-n-eks.outputs.aws_subnet_ids_public
+  min_size                = var.haproxy_scale_size
+  max_size                = var.haproxy_scale_size
+  desired_capacity        = var.haproxy_scale_size
+  health_check_type       = "EC2"
+  force_delete            = true
+
   tag { 
     key                 = "Name"
-    value               = "haproxy_devopsitall_autoscaling" 
+    value               = "haproxy_autoscaling-devops-it-all"
     propagate_at_launch = true
   }
 }
