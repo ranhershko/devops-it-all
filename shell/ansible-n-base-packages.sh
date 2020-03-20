7603155723d2431fb538ba1799fc2d9e780c2d20
Ran Hershko <ran.hershko@gmail.com>
2020-03-20 18:35:52 +0200

    Add base packages install

--- shell/ansible-n-base-packages.sh
+++ shell/ansible-n-base-packages.sh
@@ -6,5 +6,5 @@ sudo dnf -y install python2-pip
 sudo dnf -y install python3
 sudo dnf -y install python3-pip
 sudo dnf -y install jq
+sudo dnf -y install dnsmasq
 sudo -H pip3 install ansible
-sudo -H pip3 install dnsmasq
