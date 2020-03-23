2258bafb2e7f5d4df834bf27c15b6811ccf8de5e
Ran Hershko <ran.hershko@gmail.com>
2020-03-23 03:09:17 +0200

    Disable haproxy tls

--- terraform/aws/haproxy-autoscale/create-autoscale-haproxy-data.tf
+++ terraform/aws/haproxy-autoscale/create-autoscale-haproxy-data.tf
@@ -59,17 +59,17 @@ data "template_file" "backends_map_config_write_files" {
   }
 }
 
-data "template_file" "haproxy_tls_config" {
-  template = file("template/ca_bundle.crt.tpl")
-}
+#data "template_file" "haproxy_tls_config" {
+  #template = file("template/ca_bundle.crt.tpl")
+#}
 
-data "template_file" "haproxy_tls_config_write_files" {
-  template = file("template/haproxy_tls_config_write_files.tpl")
-  vars = {
-    haproxy_tls_write_file = local.haproxy_ssl_crt_config_userdata
-    haproxy_tls = join("", data.template_file.haproxy_tls_config.*.rendered)
-  }
-}
+#data "template_file" "haproxy_tls_config_write_files" {
+  #template = file("template/haproxy_tls_config_write_files.tpl")
+  #vars = {
+    #haproxy_tls_write_file = local.haproxy_ssl_crt_config_userdata
+    #haproxy_tls = join("", data.template_file.haproxy_tls_config.*.rendered)
+  #}
+#}
 
 data "template_file" "consul_client_config" {
   template = file("template/consul_client.hcl.tpl")
@@ -104,7 +104,7 @@ data "template_file" "cloud_config_all_write_files" {
     cloud_config_consul_service_write_files_data = join("", tolist(data.template_file.consul_service_config.*.rendered))
     cloud_config_resolved_conf_write_files_data = join("", tolist(data.template_file.resolved_conf_config.*.rendered))
     cloud_config_dnsmasq_consul_write_files_data = join("", tolist(data.template_file.dnsmasq_consul_config.*.rendered))
-    cloud_config_haproxy_tls_write_files_data = join("", tolist(data.template_file.haproxy_tls_config_write_files.*.rendered))
+    #cloud_config_haproxy_tls_write_files_data = join("", tolist(data.template_file.haproxy_tls_config_write_files.*.rendered))
   }
 }
 
