232b9dc58349b6806cb2d9b388911c1299471757
Ran Hershko <ran.hershko@gmail.com>
2020-03-28 14:38:24 +0300

    Add eks_worker_role_arn variable set script

---
+++ shell/set_worker_role_arn_variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export haproxy_tfvars_file=../terraform/aws/haproxy-autoscale/terraform.tfvars
+echo eks_worker_role_arn = \"$(kubectl -n kube-system get configmap aws-auth -o yaml|sed -n '/mapRoles/,/username: system:node/p' |awk 'NR>1 && NR<3'|awk '{print $3}')\" >> ${haproxy_tfvars_file}
