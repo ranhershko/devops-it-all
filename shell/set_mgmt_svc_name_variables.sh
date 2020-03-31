a1b2b8a8b527f4e9070146e7538fea571964d05d
Ran Hershko <ran.hershko@gmail.com>
2020-03-31 06:04:38 +0300

    Add kibana service name variable setting

--- shell/set_mgmt_svc_name_variables.sh
+++ shell/set_mgmt_svc_name_variables.sh
@@ -4,3 +4,4 @@ export haproxy_tfvars_file=../terraform/aws/haproxy-autoscale/terraform.tfvars
 echo jenkins_svc_name = \"$(kubectl get svc |grep  jenkins|grep -v agent|awk '{print $1}')\" > ${haproxy_tfvars_file}
 echo prometheus_server_svc_name = \"$(kubectl get svc |grep prometheus-server|awk '{print $1}')\" >> ${haproxy_tfvars_file}
 echo grafana_svc_name = \"$(kubectl get svc |grep grafana|awk '{print $1}')\" >> ${haproxy_tfvars_file}
+echo kibana_svc_name = \"$(kubectl get svc |awk '{print $1}'|grep kibana)\" >> ${haproxy_tfvars_file}
