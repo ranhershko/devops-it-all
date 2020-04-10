85b03480b47a7454d442cd9421bc8f59723c523e
Ran Hershko <ran.hershko@gmail.com>
2020-04-10 18:50:49 +0300

    Update create-k8s-secret-app-ssl to use k8s management namespace

--- shell/create-k8s-secret-app-ssl.sh
+++ shell/create-k8s-secret-app-ssl.sh
@@ -2,18 +2,22 @@
 
 app=$1
 
+kubectl create secret generic "${app}-tls" -n management \
+    --from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
+    --from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
+
 #kubectl create secret generic vault-tls -n $(namespace) \
   #--from-file="${DIR}/ca.crt" \
   #--from-file="vault.crt=${DIR}/vault-combined.crt" \
   #--from-file="vault.key=${DIR}/vault.key"
 
-if [ "$app" == "vault" ]
-then
-  kubectl create secret generic "${app}-tls" -n "${app}" \
-    --from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
-    --from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
-else
-  kubectl create secret generic "${app}-tls" -n default \
-    --from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
-    --from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
-fi
+#if [ "$app" == "vault" ]
+#then
+  #kubectl create secret generic "${app}-tls" -n "${app}" \
+    #--from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
+    #--from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
+#else
+  #kubectl create secret generic "${app}-tls" -n default \
+    #--from-file="${app}.crt=${PWD}/../ssl_created/${app}.crt" \
+    #--from-file="${app}.key=${PWD}/../ssl_created/${app}.key"
+#fi
