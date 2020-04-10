523c5e807bf0f3f6559ba776bd2473533feb330c
Ran Hershko <ran.hershko@gmail.com>
2020-04-10 18:54:39 +0300

    Update set_mgmt_svc_name_variables script to use k8s management namespace

--- shell/set_mgmt_svc_name_variables.sh
+++ shell/set_mgmt_svc_name_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export haproxy_tfvars_file=../terraform/aws/haproxy-autoscale/terraform.tfvars
-echo jenkins_svc_name = \"$(kubectl get svc |grep  jenkins|grep -v agent|awk '{print $1}')\" > ${haproxy_tfvars_file}
-echo prometheus_server_svc_name = \"$(kubectl get svc |grep prometheus-server|awk '{print $1}')\" >> ${haproxy_tfvars_file}
-echo grafana_svc_name = \"$(kubectl get svc |grep grafana|awk '{print $1}')\" >> ${haproxy_tfvars_file}
-echo kibana_svc_name = \"$(kubectl get svc |awk '{print $1}'|grep kibana)\" >> ${haproxy_tfvars_file}
+echo jenkins_svc_name = \"$(kubectl get svc --namespace=management|grep  jenkins|grep -v agent|awk '{print $1}')\" > ${haproxy_tfvars_file}
+echo prometheus_server_svc_name = \"$(kubectl get svc --namespace=management|grep prometheus-server|awk '{print $1}')\" >> ${haproxy_tfvars_file}
+echo grafana_svc_name = \"$(kubectl get svc --namespace=management|grep grafana|awk '{print $1}')\" >> ${haproxy_tfvars_file}
+echo kibana_svc_name = \"$(kubectl get svc --namespace=management|awk '{print $1}'|grep kibana)\" >> ${haproxy_tfvars_file}
