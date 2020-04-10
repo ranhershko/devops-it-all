5e6267d43f3c8b3fdc38c7a574adea631c5fb97c
Ran Hershko <ran.hershko@gmail.com>
2020-04-10 18:51:33 +0300

    modified:   ../shell/create-k8s-secret-aws-keys.sh

--- shell/create-k8s-secret-aws-keys.sh
+++ shell/create-k8s-secret-aws-keys.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-kubectl create secret generic eks-creds -n vault --from-literal="AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" --from-literal="AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
+kubectl create secret generic eks-creds --namespace=management --from-literal="AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" --from-literal="AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
