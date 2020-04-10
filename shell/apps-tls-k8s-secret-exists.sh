80dceaf681505ed3251914cfe15a5c425faaca78
Ran Hershko <ran.hershko@gmail.com>
2020-04-10 18:49:39 +0300

    Update apps-tls-k8s-secret-exists check to use k8s management namespace

--- shell/apps-tls-k8s-secret-exists.sh
+++ shell/apps-tls-k8s-secret-exists.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 app=$1
-if [ "$app" == "vault" ]
-then
-  kubectl get secret ${app}-tls -n ${app}|grep -v NAME|wc -l
-else
-  kubectl get secret ${app}-tls -n default|grep -v NAME|wc -l
-fi
+kubectl get secret ${app}-tls -n management|grep -v NAME|wc -l
+#if [ "$app" == "vault" ]
+#then
+  #kubectl get secret ${app}-tls -n ${app}|grep -v NAME|wc -l
+#else
+  #kubectl get secret ${app}-tls -n default|grep -v NAME|wc -l
+#fi
