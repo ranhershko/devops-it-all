5d4f757bb5b01d7cd0bcfa06d61c2e33f586e0a1
Ran Hershko <ran.hershko@gmail.com>
2020-04-26 11:59:24 +0300

    Add nat ip's variables for eks service communication with haproxy

--- terraform/aws/haproxy-autoscale/create-autoscale-haproxy-data.tf
+++ terraform/aws/haproxy-autoscale/create-autoscale-haproxy-data.tf
@@ -40,10 +40,11 @@ data "template_file" "haproxy_cfg_config_write_files" {
     management_server_ip       = chomp(data.http.myip.body)
     domain_name                = var.domain_name
     jenkins_svc_name           = var.jenkins_svc_name 
-    prometheus_server_svc_name = var.prometheus_server_svc_name 
     grafana_svc_name           = var.grafana_svc_name
     kibana_svc_name            = var.kibana_svc_name
-    not_nat_public_ips         = "!${chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[0])}/32 !${chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[1])}/32 !${chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[2])}/32"
+    nat_a_public_ip            = chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[0]) 
+    nat_b_public_ip            = chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[1])
+    nat_c_public_ip            = chomp(data.terraform_remote_state.vpc-n-eks.outputs.aws_vpc_nat_public_ips[2])
   }
 }
 
