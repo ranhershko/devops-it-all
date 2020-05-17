0edd31400528bd29b46a89578c725630ab7b7f65
Ran Hershko <ran.hershko@gmail.com>
2020-05-17 08:03:23 +0300

    delete unattached-kubernetes-volumes python script

---
+++ python/delete-ec2-unused-n-unattached-kubernetes-volumes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import boto3
+ec2 = boto3.resource('ec2',region_name='us-east-1')
+for vol in ec2.volumes.all():
+  if vol.state=='available':
+    need_2_delete = False
+    current_vol_name = ""
+    for tag in vol.tags:
+      if tag['Key'] == "Name":
+        current_vol_name = tag['Value']
+      if tag['Key'] == "kubernetes.io/cluster/kubernetes-devops-it-all":
+        need_2_delete = True
+    if need_2_delete:
+      print(f"Deleting {current_vol_name} volume")
+      current_vol=ec2.Volume(vol.id)
+      current_vol.delete()
+      need_2_delete = False
+      current_vol_name = ""
+      
