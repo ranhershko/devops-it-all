94eb2e4cb9398448a9f79791763eff9c3439c91e
Ran Hershko <ran.hershko@gmail.com>
2020-05-20 02:48:28 +0300

    modified:   ../shell/destroy_environment.sh

--- shell/destroy_environment.sh
+++ shell/destroy_environment.sh
@@ -7,7 +7,7 @@ cd ../../helm
 for helm_dir in `ls -la|grep ^d|tail -9|awk '{print $9}'` ; do cd $helm_dir; terraform destroy --auto-approve; cd remote_state ; terraform destroy --auto-approve; cd ../..; done
 cd ../vpc-n-eks-devops-it-all
 terraform destroy --auto-approve
-for delete_leftover_resources in `ls ../../../python/` ; do python3 ../../../python/${delete_leftover_resources} ; done
+for leftover_resources in `ls ../../../python/delete_env/` ; do python3 ../../../python/delete_env/${leftover_resources} ; done
 cd remote_state/
 terraform destroy --auto-approve
 if [ -f ~/.kube/config ] ; then rm ~/.kube/config ; fi
