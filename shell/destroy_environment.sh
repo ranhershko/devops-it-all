d8cf8c9389d83181fdac9b50e070eb0c3330dcca
Ran Hershko <ran.hershko@gmail.com>
2020-05-20 02:42:23 +0300

    destroy script

---
+++ shell/destroy_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xv
+cd terraform/aws/haproxy-autoscale/
+terraform destroy --auto-approve
+cd remote_state/
+terraform destroy --auto-approve
+cd ../../helm
+for helm_dir in `ls -la|grep ^d|tail -9|awk '{print $9}'` ; do cd $helm_dir; terraform destroy --auto-approve; cd remote_state ; terraform destroy --auto-approve; cd ../..; done
+cd ../vpc-n-eks-devops-it-all
+terraform destroy --auto-approve
+for delete_leftover_resources in `ls ../../../python/` ; do python3 ../../../python/${delete_leftover_resources} ; done
+cd remote_state/
+terraform destroy --auto-approve
+if [ -f ~/.kube/config ] ; then rm ~/.kube/config ; fi
