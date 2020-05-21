ec0dc04c9ba8902b43cbc3ecc8ac12a729f58305
Ran Hershko <ran.hershko@gmail.com>
2020-05-22 02:30:09 +0300

    Update kubectl command

--- shell/vault-operator-init-n-unseal.sh
+++ shell/vault-operator-init-n-unseal.sh
@@ -4,10 +4,10 @@ count=0
 
 for vaultServer in `kubectl get pods --namespace=management|grep vault-devopsitall-[0-9]|awk '{print $1}'`; do
   if [ ${count} -eq 0 ]; then
-    kubectl exec -ti ${vaultServer}  --namespace=management vault operator init
+    kubectl exec -ti ${vaultServer}  --namespace=management -c vault -- vault operator init
     count=1
   fi
-  kubectl exec -ti ${vaultServer} --namespace=management vault operator unseal
+  kubectl exec -ti ${vaultServer} --namespace=management -c vault -- vault operator unseal
 done
   
     
