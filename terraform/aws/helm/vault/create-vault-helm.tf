4077e95bd575795d0bfaa572166467e3f5d8abc0
Ran Hershko <ran.hershko@gmail.com>
2020-05-22 02:43:27 +0300

    modified:   terraform/aws/helm/vault/create-vault-helm.tf

--- terraform/aws/helm/vault/create-vault-helm.tf
+++ terraform/aws/helm/vault/create-vault-helm.tf
@@ -2,9 +2,6 @@ resource "helm_release" "vault" {
   name       = var.vault_helm_name
   chart      =  "${path.module}/../../../../helm/vault"
   namespace  = var.manage_namespace
-  # Support helm 2
-  version    = "v0.4.0"
-  #version    = "v0.3.3"
 
   values = [
     "${file("${path.module}/override-values.yaml")}"
