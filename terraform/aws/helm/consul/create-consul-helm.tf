a4d11ddc2d27a053cf3316a7388e3f53d5f363fe
Ran Hershko <ran.hershko@gmail.com>
2020-05-22 02:41:39 +0300

    modified:   terraform/aws/helm/consul/create-consul-helm.tf

--- terraform/aws/helm/consul/create-consul-helm.tf
+++ terraform/aws/helm/consul/create-consul-helm.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "consul" {
-  name       = var.consul_helm_name
-  chart      =  "${path.module}/../../../../helm/consul"
-  namespace  = var.manage_namespace
+  name             = var.consul_helm_name
+  chart            =  "${path.module}/../../../../helm/consul"
+  namespace        = var.manage_namespace
 
   values = [
     "${file("${path.module}/override-values.yaml")}"
