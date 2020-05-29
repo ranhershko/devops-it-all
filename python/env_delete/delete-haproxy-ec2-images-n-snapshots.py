1d7b49f77a768a57e8f8ea9267221d3b5d819429
Ran Hershko <ran.hershko@gmail.com>
2020-05-30 00:36:27 +0300

    Add AWS AMI snapshots delete

--- python/env_delete/delete-haproxy-ec2-images-n-snapshots.py
+++ python/env_delete/delete-haproxy-ec2-images-n-snapshots.py
@@ -9,10 +9,11 @@ amis = ec2.describe_images(Owners=['self'], Filters=[{'Name':'tag:Name','Values'
 for ami in amis['Images']:
   print(f"ImageId: {ami['ImageId']}")
   print(f"CreationDate: {ami['CreationDate']}")
-  #print(' '.join([ami['ImageId'], ami['CreationDate']]))
   print(f"Image tags: {ami['Tags']}")
-  #print(ami['Tags'])
   reply = str(input(f'{delete} (y/n): ')).lower().strip()
   if reply[0] == 'y':
-    print("deleting -> " + ami['ImageId'] + " - create_date = " + ami['CreationDate'])
+    print("deleting image: -> " + ami['ImageId'] + " - create_date = " + ami['CreationDate'])
     ec2.deregister_image(ImageId=ami['ImageId'])
+    for block_device_map in ami['BlockDeviceMappings']:
+      print("deleting his snapshot: -> " + block_device_map['Ebs']['SnapshotId'])
+      ec2.delete_snapshot(SnapshotId=block_device_map['Ebs']['SnapshotId'])
